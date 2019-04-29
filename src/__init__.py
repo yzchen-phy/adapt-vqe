e1ffebd983b16082717dce4194183fb3ab0ba765
Nick Mayhall <nmayhall@vt.edu>
2019-04-29 10:49:26 -0400

    added plotting scripts

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ name = "openfermion_adapt_vqe"
 import operator_pools
 import tVQE
 import vqe_methods
+import pyscf_helper 
  
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 
