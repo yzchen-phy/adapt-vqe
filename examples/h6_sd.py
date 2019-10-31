7739008ba40dcbee37810c06a97529eca2fb6e74
Nick Mayhall <nmayhall@vt.edu>
2019-10-31 17:38:02 -0400

    added files

--- examples/h6_sd.py
+++ examples/h6_sd.py
@@ -1,65 +1,70 @@
-from tVQE import *
+import scipy
 import vqe_methods 
 import operator_pools
-import pyscf_helper
-import openfermion
-import scipy
-import sys
+import pyscf_helper 
 
 import pyscf
 from pyscf import lib
 from pyscf import gto, scf, mcscf, fci, ao2mo, lo, molden, cc
 from pyscf.cc import ccsd
 
-
-from joblib import Parallel, delayed
-
-def geom_point(f):
-    r = 1.342
-    geometry = [('H',   (0,0,-r*f)), 
-                ('Be',  (0,0,0)), 
-                ('H',   (0,0,r*f))]
-    filename = "beh2_var_gsd_t9_r%04.3f.out" %f
-    sys.stdout = open(filename, 'w')
+import openfermion 
+from openfermion import *
+from tVQE import *
     
+
+def test():
+    r = 1.5
+    geometry = [('H', (0,0,1*r)), ('H', (0,0,2*r)), ('H', (0,0,3*r)), ('H', (0,0,4*r)), ('H', (0,0,5*r)), ('H', (0,0,6*r))]
+
+
     charge = 0
     spin = 0
     basis = 'sto-3g'
+
     [n_orb, n_a, n_b, h, g, mol, E_nuc, E_scf, C, S] = pyscf_helper.init(geometry,charge,spin,basis)
+
+    print(" n_orb: %4i" %n_orb)
+    print(" n_a  : %4i" %n_a)
+    print(" n_b  : %4i" %n_b)
+
     sq_ham = pyscf_helper.SQ_Hamiltonian()
     sq_ham.init(h, g, C, S)
-    
     print(" HF Energy: %12.8f" %(E_nuc + sq_ham.energy_of_determinant(range(n_a),range(n_b))))
+
     fermi_ham  = sq_ham.export_FermionOperator()
-    
+
     hamiltonian = openfermion.transforms.get_sparse_operator(fermi_ham)
-    
+
     s2 = vqe_methods.Make_S2(n_orb)
-    
+
     #build reference configuration
     occupied_list = []
     for i in range(n_a):
         occupied_list.append(i*2)
     for i in range(n_b):
         occupied_list.append(i*2+1)
-    
+
     print(" Build reference state with %4i alpha and %4i beta electrons" %(n_a,n_b), occupied_list)
     reference_ket = scipy.sparse.csc_matrix(openfermion.jw_configuration_state(occupied_list, 2*n_orb)).transpose()
-    
+
     [e,v] = scipy.sparse.linalg.eigsh(hamiltonian.real,1,which='SA',v0=reference_ket.todense())
     for ei in range(len(e)):
         S2 = v[:,ei].conj().T.dot(s2.dot(v[:,ei]))
-        print(" FCI State %4i: %12.8f au  <S2>: %12.8f" %(ei,e[ei]+E_nuc,S2))
-    fermi_ham += openfermion.FermionOperator((),E_nuc)
+        print(" State %4i: %12.8f au  <S2>: %12.8f" %(ei,e[ei]+E_nuc,S2))
+    
+    fermi_ham += FermionOperator((),E_nuc)
     pyscf.molden.from_mo(mol, "full.molden", sq_ham.C)
-    
-    pool = operator_pools.spin_complement_GSD()
-    pool.init(n_orb)
-    
-    
-    [e,v,params] = vqe_methods.adapt_vqe(fermi_ham, pool, reference_ket, adapt_thresh=1e-6, theta_thresh=1e-9)
-    
+
+    #   Francesco, change this to singlet_GSD() if you want generalized singles and doubles
+    pool = operator_pools.singlet_SD()
+    pool.init(n_orb, n_occ_a=n_a, n_occ_b=n_b, n_vir_a=n_orb-n_a, n_vir_b=n_orb-n_b)
+
+    [e,v,params] = vqe_methods.adapt_vqe(fermi_ham, pool, reference_ket, theta_thresh=1e-9)
+
     print(" Final ADAPT-VQE energy: %12.8f" %e)
     print(" <S^2> of final state  : %12.8f" %(v.conj().T.dot(s2.dot(v))[0,0].real))
 
-Parallel(n_jobs=20)(delayed(geom_point)(f/10+.5) for f in range(20,21))
+
+if __name__== "__main__":
+    test()
