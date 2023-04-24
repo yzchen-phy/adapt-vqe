c8e3f927f788af1c8bcff7596f465c364de781e5
yzchen-phy <yanzhucyz@gmail.com>
2023-04-24 11:15:38 -0400

    update __init__py to include code for adapt-qaoa

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,12 @@
 name = "openfermion_adapt_vqe"
 
 import operator_pools
+import operator_pools_qaoa
+import oeprator_pools_pauli
 import tVQE
 import vqe_methods
+import qaoa_methods_es
+import qaoa_methods_spsa
 import pyscf_helper 
  
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
